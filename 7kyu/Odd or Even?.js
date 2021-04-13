dcb8f80afeaaab8adfba4d8bb01544ef0ca14658
Raquel Rodriguez <srtademaciel@gmail.com>
2021-04-13 19:06:32 +0200

    Kata 7kyu

---
+++ 7kyu/Odd or Even?.js
@@ -0,0 +1,12 @@
+//Intructions
+//Given a list of integers, determine whether the sum of its elements is odd or even.
+//Give your answer as a string matching "odd" or "even".
+//If the input array is empty consider it as: [0] (array with a zero).
+
+const oddOrEven = (array) => {
+    //enter code here
+   let sum = array.reduce((acc, number) => {
+     return acc + number
+   }, 0)
+    return sum%2==0 ? 'even' : 'odd'
+ }
